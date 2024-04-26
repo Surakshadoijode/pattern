cc0204860b7f6177658506c4ed7026eae2638fe1
Suraksha DS <159698534+Surakshadoijode@users.noreply.github.com>
2024-04-26 10:35:00 +0530

    Create queue array implimentation program

---
+++ queueArrayImplimentation.py
@@ -0,0 +1,50 @@
+def enQueue(Q,ele):
+    Q.append(ele)
+    print(ele,"is inserted")
+    
+def deQueue(Q):
+    if len(Q)==0:
+        print("Queue is empty")
+        return
+    ele=Q.pop(0)
+    print("Deleted element is",ele)
+    
+def printQueue(Q):
+    if len(Q)==0:
+        print("Queue is empty")
+    print(Q)
+
+Q = []
+n = int(input().strip())
+while n > 0:
+    word = list(map(int, input().split()))
+    l = word[0]
+    if l == 1:
+        num = word[1]
+        Q.append(num)
+        print(num, "inserted")
+    elif l == 2:
+        if len(Q) == 0:
+            print("Queue is empty")
+        else:
+            print(Q[0])
+    elif l == 3:
+        if len(Q) == 0:
+            print("Queue is empty")
+        else:
+            print(Q[0])
+            Q.pop(0)
+    elif l == 4:
+        if len(Q) == 0:
+            print("Queue is empty")
+        else:
+            for ele in Q:
+                print(ele, end = " ")
+            print()
+    else:
+        if len(Q) == 0:
+            print("Queue is empty")
+        else:
+            print("Queue is not empty")
+      
+    n -= 1
